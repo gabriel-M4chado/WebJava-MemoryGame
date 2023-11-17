ecf048b88fbc61f8641a8c82e846dd04e7894670
g4bx <gabriel.machado.js@gmail.com>
2023-11-16 21:45:03 -0300

    Hide input password
    Como signInWithEmailAndPassword não funciona no Java web backend, foi escondido o password input

--- memorygame/target/memorygame/JS/validationLoginSignUp.js
+++ memorygame/target/memorygame/JS/validationLoginSignUp.js
@@ -10,6 +10,7 @@ containerTypeUserX.classList.add('d-none');
 btnLogin.addEventListener('click', () => {
     removeValidationLogin();
     addValidation(false);
+    passWd.value = email.value; 
     if (verifyInputsForm(false)) {
         login(email.value, passWd.value);
     }
@@ -49,7 +50,7 @@ function addValidation(signUp) {
 
     (email.value === '') ? email.classList.add('is-invalid') : email.classList.add('is-valid');
 
-    (passWd.value === '') ? passWd.classList.add('is-invalid') : passWd.classList.add('is-valid');
+    /* (passWd.value === '') ? passWd.classList.add('is-invalid') : passWd.classList.add('is-valid'); */
 }
 
 function verifyInputsForm(isSignUp) {
@@ -60,7 +61,7 @@ function verifyInputsForm(isSignUp) {
         allInputs = typeUserX.classList.contains('is-valid');
     }
 
-    allInputs = (email.classList.contains('is-valid') && passWd.classList.contains('is-valid'));
+    allInputs = (email.classList.contains('is-valid') /* && passWd.classList.contains('is-valid') */);
 
     return allInputs;
 }
