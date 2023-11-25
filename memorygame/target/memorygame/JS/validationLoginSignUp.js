c9945ad8387857e2929581446c258235079d1109
g4bx <gabriel.machado.js@gmail.com>
2023-11-25 14:36:19 -0300

    Liberando o input do password na inscrição

--- memorygame/target/memorygame/JS/validationLoginSignUp.js
+++ memorygame/target/memorygame/JS/validationLoginSignUp.js
@@ -2,15 +2,13 @@ const email = document.getElementById('typeEmailX');
 const passWd = document.getElementById('typePasswordX');
 const btnLogin = document.getElementById('btnLogin');
 const btnSignUP = document.getElementById('btnSignUP');
-const containerTypeUserX = document.getElementById('containerTypeUserX');
 
 btnSignUP.classList.add('d-none');
-containerTypeUserX.classList.add('d-none');
 
 btnLogin.addEventListener('click', () => {
     removeValidationLogin();
     addValidation(false);
-    passWd.value = email.value; 
+    passWd.value = email.value;
     if (verifyInputsForm(false)) {
         login(email.value, passWd.value, 'login');
     }
@@ -20,11 +18,13 @@ const btnSignUPText = document.getElementById('btnSignUPText');
 btnSignUPText.addEventListener('click', () => {
     const titleLoginScreen = document.getElementById('titleLoginScreen');
     const textSigup = document.querySelector('.mb-0');
+    const containerTypePasswordX = document.querySelector('#containerTypePasswordX');
+
     removeValidationLogin();
 
     btnLogin.style.display = 'none';
     btnSignUP.classList.remove('d-none');
-    containerTypeUserX.classList.remove('d-none');
+    containerTypePasswordX.classList.remove('d-none');
     titleLoginScreen.textContent = 'sing up';
     textSigup.classList.add('d-none');
     document.querySelector('#textDataLogin').textContent = 'Enter your login details';
@@ -33,6 +33,9 @@ btnSignUPText.addEventListener('click', () => {
 btnSignUP.addEventListener('click', () => {
     addValidation(true);
     document.querySelector('.mb-0').classList.add('d-none');
+    if (verifyInputsForm(false)) {
+        login(email.value, passWd.value, 'login');
+    }
 });
 
 
@@ -45,24 +48,22 @@ function removeValidationLogin() {
 
 function addValidation(signUp) {
     if (signUp) {
-        const typeUserX = document.getElementById('typeUserX');
-        (typeUserX.value === '') ? typeUserX.classList.add('is-invalid') : typeUserX.classList.add('is-valid');
+        (passWd.value === '') ? passWd.classList.add('is-invalid') : passWd.classList.add('is-valid');
     }
 
     (email.value === '') ? email.classList.add('is-invalid') : email.classList.add('is-valid');
 
-    /* (passWd.value === '') ? passWd.classList.add('is-invalid') : passWd.classList.add('is-valid'); */
+
 }
 
 function verifyInputsForm(isSignUp) {
     let allInputs = false;
 
-    if (isSignUp) {
-        const typeUserX = document.getElementById('typeUserX');
-        allInputs = typeUserX.classList.contains('is-valid');
-    }
+    allInputs = (email.classList.contains('is-valid'));
 
-    allInputs = (email.classList.contains('is-valid') /* && passWd.classList.contains('is-valid') */);
+    if (isSignUp) {
+        allInputs = passWd.classList.contains('is-valid');
+    }
 
     return allInputs;
 }
